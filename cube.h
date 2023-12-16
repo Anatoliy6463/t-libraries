99165be6135442eb63235fe610c15c655a574da8
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-12-16 20:51:38 +0300

    Create cube.h

---
+++ cube.h
@@ -0,0 +1,21 @@
+int cubemake(int a, char sym, char symbol[a][a])
+{
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; i < a; j++)
+    	{
+			symbol[i][j] = sym;
+    	}
+	}
+  	cubeprint(a, sym, symbol);
+}
+int cubeprint(int a, char sym, char symbol[a][a])
+{
+	for (int i = 0; i < a; i++)
+	{
+    	for (int j = 0; i < a; j++)
+    	{
+    	  printf("%c", symbol[i][j]);
+    	}
+	}
+}
